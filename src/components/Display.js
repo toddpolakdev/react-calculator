117f71ca8dcb3acca30c71c27f2711530799b686
Todd Polak <toddpolakdev@gmail.com>
2025-08-29 09:00:55 -0500

    basic features

--- src/components/Display.js
+++ src/components/Display.js
@@ -17,20 +17,3 @@ function Display({ display, error, operation, previousValue }) {
 }
 
 export default Display;
-
-// import React from "react";
-
-// function Display({ display, result }) {
-//   console.log("display", display);
-//   console.log("result", result);
-//   return (
-//     <div className="">
-//       {/* <div className="display">{display}</div> */}
-//       <div className="result">
-//         {result ? `${display} = ${result}` : display && `${display}`}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Display;
